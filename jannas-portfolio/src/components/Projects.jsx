23f3ddb2fe5a0c4b46fe5872c08dd4d9511d8f29
Janna Almuqaisib <jannakalmuqaisib@gmail.com>
2024-11-03 21:35:00 +0300

    added card animation dunno if i like it

--- jannas-portfolio/src/components/Projects.jsx
+++ jannas-portfolio/src/components/Projects.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import github from '../assets/github.png';
 
 function Projects() {
   return (
     <div className="h-screen content-center">
-        <h1 className="text-4xl font-bold text-center">My Projects</h1>
+        <div class="container">
+         <div class="card__container">
+            <article class="card__article">
+               <img src={github} alt="image" class="card__img" />
+
+               <div class="card__data">
+                  <span class="card__description">Vancouver Mountains, Canada</span>
+                  <h2 class="card__title">The Great Path</h2>
+                  <a href="#" class="card__button">Read More</a>
+               </div>
+            </article>
+
+            <article class="card__article">
+               <img src={github} alt="image" class="card__img" />
+
+               <div class="card__data">
+                  <span class="card__description">Poon Hill, Nepal</span>
+                  <h2 class="card__title">Starry Night</h2>
+                  <a href="#" class="card__button">Read More</a>
+               </div>
+            </article>
+
+            <article class="card__article">
+               <img src={github} alt="image" class="card__img" />
+
+               <div class="card__data">
+                  <span class="card__description">Bojcin Forest, Serbia</span>
+                  <h2 class="card__title">Path Of Peace</h2>
+                  <a href="#" class="card__button">Read More</a>
+               </div>
+            </article>
+         </div>
+      </div>
     </div>
   )
 }
